2a88419442c9bdb36a0b1d4329dbb70ef1073420
jpbai@work <software_bjp@126.com>
2015-12-24 20:55:41 +0800

    tmp solve 268

---
+++ Leetcode 268-Missing Number.py
@@ -0,0 +1,26 @@
+# -*- coding:utf8 -*-
+"""
+url:https://leetcode.com/problems/missing-number/
+run time:0 ms
+tips:
+1.由于题目中没有说明给定的数组是有序的，因此遍历直到最后一个数字之前，都不能够知道丢失的是哪个数字
+"""
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+
+        
+
+def test():
+    s = Solution()
+    for n in range(1, 10):
+        print s.canWinNim(n)
+
+if __name__ == '__main__':
+    test()
