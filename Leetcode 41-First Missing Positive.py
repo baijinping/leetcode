37362c814aaab5cd6ea8346f922a7337b1021bfd
jpbai@work <software_bjp@126.com>
2015-12-28 19:54:16 +0800

    solve 41 with better solution

--- Leetcode 41-First Missing Positive.py
+++ Leetcode 41-First Missing Positive.py
@@ -1,9 +1,11 @@
 # -*- coding:utf8 -*-
 """
 url:https://leetcode.com/problems/first-missing-positive/
-run time:48 ms(bad), 
+run time:48 ms(bad), 48 ms(better)
 tips:
-
+在这道题卡了很久，想方设法找规律，还是纠结在数组的无序性上
+最终还是在http://blog.csdn.net/nanjunxiao/article/details/12973173看了解法
+原来就差一步，将参数数组本身当做桶，使得数组正数元素在下一次遍历时保持有序接口即可
 """
 
 
@@ -42,34 +44,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # 统计正数数量，并找到最大最小值
-        positive_count = 0
-        max_n = 0
-        min_n = 0
-        for n in nums:
-            if n < 0:
-                continue
-            positive_count += 1
-            if n < min_n:
-                min_n = n
-            elif n > max_n:
-                max_n = n
-        
-        # 若没有正数，或最小正数大于1，则返回1
-        if positive_count == 0 or min_n > 1:
+        len_of_nums = len(nums)
+        if len_of_nums == 0:
             return 1
 
-        #
-        # *暂不考虑有重复数字*
-        #
-        # 若所有正数都是连续的，则返回最大正数+1
-        if positive_count == max_n:
-            return max_n + 1
+        # 第一次遍历，将正数放到与其值对应的位置上
+        n = 0
+        for i in xrange(len_of_nums):
+            n = nums[i]
 
-        # TODO
-        # 奇数计数/偶数计数是否有用？
+            tmp = 0
+            while 1:
+                if not 0 < n < len_of_nums or n == nums[n - 1]:
+                    break
 
-        
+                tmp = nums[n - 1]
+                nums[n - 1], nums[i], n = n, tmp, tmp
+
+        # 第二次遍历，找到第一个非正数所在位置，返回即可
+        for i, n in enumerate(nums):
+            if i + 1 != n or n <= 0:
+                return i + 1
+        else:
+            return n + 1
 
 
 def test():
@@ -77,7 +74,12 @@ def test():
     test_cases = [
         ([1, 2, 0], 3),
         ([3, 4, 1, -1], 2),
-        ([2], 1)
+        ([2], 1),
+        ([1, 1], 2),
+        ([3, 1], 2),
+        ([1], 2),
+        ([2, 1], 3),
+        ([], 1)
     ]
     for nums, miss_num in test_cases:
         if s.firstMissingPositive(nums) != miss_num:
